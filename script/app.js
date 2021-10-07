19e7476b9f40c37c55647e85af5836fb85e8512d
alijon <alijonfarmonov54@gmail.com>
2021-10-07 18:50:45 +0500

    alijon

---
+++ script/app.js
@@ -0,0 +1,12 @@
+  
+  let name = prompt("Ismizgizni kiriting")
+  
+  
+let a = 'https://webprograma.github.io/financiary/'
+  
+do {
+    let = password = +prompt("parolni kiriting")
+} while (password !== 1990);
+    alert("Parol to'ri <<OK>> ni bosib saytga kirishingiz mumkin🙃")
+    
+    
