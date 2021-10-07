cd19ac44cff730fbe89e4c61ff6b7564e252ca81
alijon <alijonfarmonov54@gmail.com>
2021-10-07 19:23:18 +0500

    alijon

--- script/app.js
+++ script/app.js
@@ -2,8 +2,6 @@
   let name = prompt("Ismizgizni kiriting")
   
   
-let a = 'https://webprograma.github.io/financiary/'
-  
 do {
     let = password = +prompt("parolni kiriting")
 } while (password !== 1990);
